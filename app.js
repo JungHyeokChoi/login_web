1e59e013e20452f26d2341df02124b16ac7ca96f
JungHyeok Choi <page1114@gamil.com>
2020-06-30 17:32:04 +0900

    Login

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var bodyParser = require('body-parser')
+var apiRouter = require('./routes/Router')
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+const port = process.env.PORT
+
+//MongoDB Connection
+var pw = process.env.PASSWORD
+var url = `mongodb+srv://root:${pw}@cluster0.lkryw.mongodb.net/MyDB?retryWrites=true&w=majority`
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// process.cwd() 현재 경로 찾기
+app.set('views', path.resolve(__dirname + '/views'))
+app.set('view engine', 'ejs')
+
+// Error : app.set('view engine', 'html')
+app.engine('html', require('ejs').renderFile);
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
+app.use('/', apiRouter)
+
+
+app.listen(port, () => {
+    console.log(`Server is Starting at http://localhost:${port}`)
+})
---
+++ routes/Router.js
@@ -0,0 +1,83 @@
+var express = require('express')
+var router = express.Router()
+var User = require('../models/User.js')
+
+var name = {
+    a : "CJH",
+    b : "PEJ",
+    c : "NoName"
+}
+
+router.route('/')
+    .get((req, res, next) => {
+        User.find((err, result) => {
+            if(err){
+             console.log(err.body)
+         }
+         //res.send(result)
+         res.render('index.ejs', {data:result.username})
+        })
+    })
+    .post((req, res) => {
+        var contact = new User()
+
+        contact.username = req.body.username
+        contact.passwordHash = req.body.passwordHash
+        contact.email = req.body.email
+
+        contact.save((err, result) => {
+            if(err){
+                console.log(err.body)
+            }
+            console.log(result)
+            res.send("Success")
+        })
+    })
+
+router.route('/signup')
+    .get((req, res, next) =>{
+        res.render('signup.ejs')
+    })
+    .post((req,res,next) => {
+        var contact = new User()
+
+        contact.username = req.body.username
+        contact.passwordHash = req.body.passwordHash
+        contact.email = req.body.email
+        
+        contact.save((err, result) => {
+            if(err){
+                console.log(err.body)
+            }
+            console.log(result)
+            res.redirect(`/login`)
+        })
+    })
+
+router.route('/login')
+    .get((req, res, next) => {
+        res.render('login.ejs')
+    })
+    .post(async (req, res, next) => {
+        var username = await req.body.username
+        var passwordHash = await req.body.passwordHash
+        
+        User.findOne({username:username} ,(err,result) =>{
+            if(err){
+                console.log(err.body)
+            }
+            if(!result){
+                res.send(`${username} is not exist`)
+            } else {
+                if(result.passwordHash == passwordHash){
+                    console.log(username)
+                    res.render('index.ejs', {data:result.username})
+                }
+                else{
+                    res.send(`${username}'s password is wrong`)
+                }
+            }
+        })
+    })
+
+module.exports = router;
