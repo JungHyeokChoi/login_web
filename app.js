fac4ca5156bd1bd475ce4046085efb60f1e25676
JungHyeok Choi <page1114@gamil.com>
2020-07-01 14:42:24 +0900

    Setting Passport

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ mongoose.Promise = global.Promise
 var flash = require('express-flash-messages')
 var cookieParser = require('cookie-parser')
 var session = require('express-session')
+var passport = require('passport')
+var localStrategy = require('passport-local')
+var User = require('./models/User')
 
 require('dotenv').config()
 const port = process.env.PORT
@@ -27,12 +30,39 @@ app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
-app.use('/', apiRouter)
 app.use(session({
     secret : "3ojhtpioteuws90t",
     resave : true,
     saveUninitialized : true
 }))
+app.use(flash())
+
+//Passport
+app.use(passport.initialize())
+app.use(passport.session())
+passport.serializeUser((user, done) => {
+    done(null, user_id)
+})
+passport.deserializeUser((userId, done) => {
+    User.findById(userId, (err, result) => {
+        done(err, result)
+    })
+})
+var local = new localStrategy((username, password, done) => {
+    User.findOne({username})
+    .then(user=>{
+        if(!user || user.vailPassword(password)){
+            done(null, false, {message : "Invaild username password"})
+        } else {
+            done()
+        }
+    })
+    .catch(e => done(e))
+})
+passport.use("local", local)
+
+//Add Routing File List on Midlware
+app.use('/', apiRouter)
 
 app.listen(port, () => {
     console.log(`Server is Starting at http://localhost:${port}`)
